4d27687bc3c5eab01d99d03ea13018c5ed5a86d4
Mohammed Moinuddin <7moinuddin5@gmail.com>
2025-07-16 14:43:34 +0530

    Added validations

--- shortner/src/main/java/com/url/shortner/DTOs/RegisterrequestDTO.java
+++ shortner/src/main/java/com/url/shortner/DTOs/RegisterrequestDTO.java
@@ -2,12 +2,19 @@ package com.url.shortner.DTOs;
 
 import java.util.Set;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterrequestDTO {
+    @NotBlank(message = "username is required")
     private String username;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
+    @Size(min = 8, message = "Password must have at least 8 characters")
     private String password;
     private Set<String> roles;    
 
